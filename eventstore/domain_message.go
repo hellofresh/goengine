2675595dd1d6ba0e6af9955b6cf2f8e783aabcf2
Italo Lelis de Vietro <italolelis@gmail.com>
2016-10-29 14:04:55 +0200

    Added json notation to the domain message

--- eventstore/domain_message.go
+++ eventstore/domain_message.go
@@ -3,10 +3,10 @@ package eventstore
 import "time"
 
 type DomainMessage struct {
-	ID         string
-	version    int
-	payload    DomainEvent
-	recordedOn time.Time
+	ID         string      `json:"aggregate_id,omitempty"`
+	Version    int         `json:"version"`
+	Payload    DomainEvent `json:"payload"`
+	RecordedOn time.Time   `json:"recorded_on"`
 }
 
 func NewDomainMessage(id string, version int, payload DomainEvent, recordedOn time.Time) *DomainMessage {
