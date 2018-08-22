8596b4d80dce3477eb6c89461ef3de5dd55b4d21
warnar boekkooi <wbo@hellofresh.com>
2018-08-22 14:59:07 +0200

    Add PayloadFactory mock
    In order to test the MessageFactory we require a mock for the PayloadFactory.

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ package goengine_dev
 
 // In order to make sure that we have the same mocks we can regenerate them using `go generate`
 //go:generate go run vendor/github.com/vektra/mockery/cmd/mockery/mockery.go -dir eventstore/ -name=EventStore -outpkg mocks -output ./mocks -case underscore
+//go:generate go run vendor/github.com/vektra/mockery/cmd/mockery/mockery.go -dir eventstore/ -name=PayloadFactory -outpkg mocks -output ./mocks -case underscore
 //go:generate go run vendor/github.com/vektra/mockery/cmd/mockery/mockery.go -dir messaging/ -name=Message -outpkg mocks -output ./mocks -case underscore
 
 // blank imports help docs.
