0041c1257bcac123ee12c58a9d942ace577824c3
warnar boekkooi <wbo@hellofresh.com>
2018-11-22 16:15:04 +0100

    Add AggregateProjector SQL
    Before we implemented the AggregateProjector let's be clear on the schema.

--- eventstore/postgres/projector_sql.go
+++ eventstore/postgres/projector_sql.go
@@ -7,10 +7,7 @@ import (
 	"github.com/lib/pq"
 )
 
-// StreamProjectorCreateSchema return the sql statement needed for the postgres database in order to use the StreamProjector
-func StreamProjectorCreateSchema(projectionTable string, streamName eventstore.StreamName, streamTable string) []string {
-	statements := make([]string, 3)
-	statements[0] = `CREATE FUNCTION public.event_stream_notify ()
+const sqlFuncEventStreamNotify = `CREATE FUNCTION public.event_stream_notify ()
   RETURNS TRIGGER
 LANGUAGE plpgsql AS $$
 DECLARE
@@ -27,26 +24,18 @@ BEGIN
 END;
 $$;`
 
-	statements[1] = fmt.Sprintf(
-		`CREATE TABLE IF NOT EXISTS %s (
-			no SERIAL,
-			name VARCHAR(150) NOT NULL,
-			position BIGINT NOT NULL DEFAULT 0,
-			state JSONB NOT NULL DEFAULT ('{}'),
-			PRIMARY KEY (no),
-			UNIQUE (name)
-		)`,
-		pq.QuoteIdentifier(projectionTable),
-	)
-
-	triggerName := fmt.Sprintf("%s_notify", streamTable)
-	statements[2] = fmt.Sprintf(
+// sqlTriggerEventStreamNotify a helper to create the sql on a event store table
+func sqlTriggerEventStreamNotifyTemplate(eventStreamName eventstore.StreamName, eventStreamTable string) string {
+	triggerName := fmt.Sprintf("%s_notify", eventStreamTable)
+	return fmt.Sprintf(
 		`DO LANGUAGE plpgsql $$
 		 BEGIN
 		   IF NOT EXISTS(
 		       SELECT * FROM information_schema.triggers
 		       WHERE
+		           event_object_schema = 'public' AND
 		           event_object_table = %s AND
+		           trigger_schema = 'public' AND
 		           trigger_name = %s
 		   )
 		   THEN
@@ -58,12 +47,49 @@ $$;`
 		   END IF;
 		 END;
 		 $$`,
-		quoteString(streamTable),
+		quoteString(eventStreamTable),
 		quoteString(triggerName),
 		pq.QuoteIdentifier(triggerName),
-		pq.QuoteIdentifier(streamTable),
-		quoteString(string(streamName)),
+		pq.QuoteIdentifier(eventStreamTable),
+		quoteString(string(eventStreamName)),
 	)
+}
 
-	return statements
+// StreamProjectorCreateSchema return the sql statement needed for the postgres database in order to use the StreamProjector
+func StreamProjectorCreateSchema(projectionTable string, streamName eventstore.StreamName, streamTable string) []string {
+	return []string{
+		sqlFuncEventStreamNotify,
+		sqlTriggerEventStreamNotifyTemplate(streamName, streamTable),
+
+		fmt.Sprintf(
+			`CREATE TABLE IF NOT EXISTS %s (
+				no SERIAL,
+				name VARCHAR(150) NOT NULL,
+				position BIGINT NOT NULL DEFAULT 0,
+				state JSONB NOT NULL DEFAULT ('{}'),
+				PRIMARY KEY (no),
+				UNIQUE (name)
+			)`,
+			pq.QuoteIdentifier(projectionTable),
+		),
+	}
+}
+
+// AggregateProjectorCreateSchema return the sql statement needed for the postgres database in order to use the AggregateProjector
+func AggregateProjectorCreateSchema(projectionTable string, streamName eventstore.StreamName, streamTable string) []string {
+	return []string{
+		sqlFuncEventStreamNotify,
+		sqlTriggerEventStreamNotifyTemplate(streamName, streamTable),
+
+		fmt.Sprintf(
+			`CREATE TABLE IF NOT EXISTS %s (
+				no SERIAL,
+				aggregate_id UUID UNIQUE NOT NULL,
+				position BIGINT NOT NULL DEFAULT 0,
+  				state JSONB,
+  				PRIMARY KEY (no)
+			)`,
+			pq.QuoteIdentifier(projectionTable),
+		),
+	}
 }
