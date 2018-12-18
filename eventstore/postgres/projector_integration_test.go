94329bcbb348b020b12c626cf5f85a55d4f97fe3
warnar boekkooi <wbo@hellofresh.com>
2018-12-18 18:26:36 +0100

    Extract shared integration tests code
    Since a lot of code regarding integration tests for projectors will be shared it's time to extract it into a nice little suite that all of them can use.
    This include the setup of the eventStore and the appending of events.
    
    As a added bonus we also moved the domain used for the integration tests into the same file.

---
+++ eventstore/postgres/projector_integration_test.go
@@ -0,0 +1,213 @@
+// +build integration
+
+package postgres_test
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"sync"
+	"time"
+
+	eventStoreJSON "github.com/hellofresh/goengine/eventstore/json"
+	"github.com/hellofresh/goengine/eventstore/postgres"
+	eventStoreSQL "github.com/hellofresh/goengine/eventstore/sql"
+
+	"github.com/hellofresh/goengine/aggregate"
+	"github.com/hellofresh/goengine/internal/test"
+	"github.com/hellofresh/goengine/metadata"
+
+	"github.com/hellofresh/goengine/eventstore"
+	"github.com/hellofresh/goengine/messaging"
+)
+
+var _ eventstore.Projection = &DepositedProjection{}
+
+type (
+	AccountCredited struct {
+		Amount uint
+	}
+
+	AccountDeposited struct {
+		Amount uint
+	}
+
+	DepositedProjection struct {
+	}
+
+	depositedProjectionState struct {
+		Total       uint
+		TotalAmount uint64
+	}
+)
+
+func (p *DepositedProjection) Init(ctx context.Context) (interface{}, error) {
+	return depositedProjectionState{}, nil
+}
+
+func (p *DepositedProjection) Name() string {
+	return "deposited_report"
+}
+
+func (p *DepositedProjection) FromStream() eventstore.StreamName {
+	return "event_stream"
+}
+
+func (p *DepositedProjection) Handlers() map[string]eventstore.ProjectionHandler {
+	return map[string]eventstore.ProjectionHandler{
+		"account_debited": func(ctx context.Context, state interface{}, message messaging.Message) (interface{}, error) {
+			projectionState := state.(depositedProjectionState)
+
+			switch event := message.Payload().(type) {
+			case AccountDeposited:
+				projectionState.Total++
+				projectionState.TotalAmount += uint64(event.Amount)
+			default:
+				return state, errors.New("unexpected message payload type")
+			}
+
+			return projectionState, nil
+		},
+	}
+}
+
+func (p *DepositedProjection) ReconstituteState(data []byte) (interface{}, error) {
+	var state depositedProjectionState
+	if err := json.Unmarshal(data, &state); err != nil {
+		return nil, err
+	}
+
+	return state, nil
+}
+
+func (p *DepositedProjection) Reset(ctx context.Context) error {
+	return nil
+}
+
+func (p *DepositedProjection) Delete(ctx context.Context) error {
+	return nil
+}
+
+type projectorSuite struct {
+	test.PostgresSuite
+
+	eventStream        eventstore.StreamName
+	eventStore         *postgres.EventStore
+	eventStoreTable    string
+	payloadTransformer *eventStoreJSON.PayloadTransformer
+}
+
+func (s *projectorSuite) SetupTest() {
+	s.PostgresSuite.SetupTest()
+	db := s.DB()
+
+	s.eventStream = eventstore.StreamName("event_stream")
+
+	// Create payload transformer
+	s.payloadTransformer = eventStoreJSON.NewPayloadTransformer()
+
+	// Use a persistence strategy
+	persistenceStrategy, err := postgres.NewPostgresStrategy(s.payloadTransformer)
+	s.Require().NoError(err, "failed initializing persistent strategy")
+
+	// Create message factory
+	messageFactory, err := eventStoreSQL.NewAggregateChangedFactory(s.payloadTransformer)
+	s.Require().NoError(err, "failed on dependencies load")
+
+	// Create event store
+	s.eventStore, err = postgres.NewEventStore(persistenceStrategy, db, messageFactory, nil)
+	s.Require().NoError(err, "failed on dependencies load")
+
+	// Create the event stream
+	ctx := context.Background()
+	err = s.eventStore.Create(ctx, s.eventStream)
+	s.Require().NoError(err, "failed on create event stream")
+
+	// Setup the projection tables etc.
+	s.eventStoreTable, err = persistenceStrategy.GenerateTableName(s.eventStream)
+	s.Require().NoError(err, "failed to generate eventstream table name")
+}
+
+func (s *projectorSuite) TearDownTest() {
+	s.eventStore = nil
+	s.eventStream = ""
+	s.eventStoreTable = ""
+	s.payloadTransformer = nil
+
+	s.PostgresSuite.TearDownTest()
+}
+
+func (s *projectorSuite) appendEvents(events map[aggregate.ID][]interface{}) {
+	ctx := context.Background()
+	for aggID, aggEvents := range events {
+		// Find the last event
+		matcher := metadata.WithConstraint(
+			metadata.WithConstraint(
+				metadata.NewMatcher(),
+				aggregate.TypeKey,
+				metadata.Equals,
+				"account",
+			),
+			aggregate.IDKey,
+			metadata.Equals,
+			aggID,
+		)
+		stream, err := s.eventStore.Load(ctx, s.eventStream, 0, nil, matcher)
+		s.Require().NoError(err)
+
+		var lastVersion int
+		for stream.Next() {
+			msg, _, err := stream.Message()
+			s.Require().NoError(err)
+
+			lastVersion = int(msg.Metadata().Value(aggregate.VersionKey).(float64))
+		}
+		s.Require().NoError(stream.Err())
+
+		// Transform the events into messages
+		messages := make([]messaging.Message, len(aggEvents))
+		for i, event := range aggEvents {
+			m := metadata.WithValue(
+				metadata.WithValue(
+					metadata.WithValue(metadata.New(), aggregate.IDKey, aggID),
+					aggregate.VersionKey,
+					lastVersion+i+1,
+				),
+				aggregate.TypeKey,
+				"account",
+			)
+
+			message, err := aggregate.ReconstituteChange(
+				aggID,
+				messaging.GenerateUUID(),
+				event,
+				m,
+				time.Now().UTC(),
+				uint(i+1),
+			)
+			s.Require().NoError(err, "failed on create messages")
+
+			messages[i] = message
+		}
+
+		// Append the messages to the stream
+		err = s.eventStore.AppendTo(ctx, s.eventStream, messages)
+		s.Require().NoError(err, "failed to append messages")
+	}
+}
+
+// waitTimeout waits for the waitgroup for the specified max timeout.
+// Returns true if waiting timed out.
+func (s *projectorSuite) waitTimeout(wg *sync.WaitGroup, timeout time.Duration) bool {
+	c := make(chan struct{})
+	go func() {
+		defer close(c)
+		wg.Wait()
+	}()
+	select {
+	case <-c:
+		return false // completed normally
+	case <-time.After(timeout):
+		return true // timed out
+	}
+}
--- eventstore/postgres/projector_stream_integration_test.go
+++ eventstore/postgres/projector_stream_integration_test.go
@@ -5,32 +5,20 @@ package postgres_test
 import (
 	"context"
 	"database/sql"
-	"encoding/json"
-	"errors"
 	"runtime"
 	"sync"
 	"testing"
 	"time"
 
 	"github.com/hellofresh/goengine/aggregate"
-	"github.com/hellofresh/goengine/eventstore"
-	eventStoreJSON "github.com/hellofresh/goengine/eventstore/json"
 	"github.com/hellofresh/goengine/eventstore/postgres"
-	eventStoreSQL "github.com/hellofresh/goengine/eventstore/sql"
-	"github.com/hellofresh/goengine/internal/test"
-	"github.com/hellofresh/goengine/messaging"
-	"github.com/hellofresh/goengine/metadata"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 )
 
 type (
 	streamProjectorTestSuite struct {
-		test.PostgresSuite
-
-		eventStream        eventstore.StreamName
-		eventStore         *postgres.EventStore
-		payloadTransformer *eventStoreJSON.PayloadTransformer
+		projectorSuite
 	}
 )
 
@@ -39,38 +27,12 @@ func TestStreamProjectorSuite(t *testing.T) {
 }
 
 func (s *streamProjectorTestSuite) SetupTest() {
-	s.PostgresSuite.SetupTest()
-	db := s.DB()
+	s.projectorSuite.SetupTest()
 
-	s.eventStream = eventstore.StreamName("event_stream")
-
-	// Create payload transformer
-	s.payloadTransformer = eventStoreJSON.NewPayloadTransformer()
-
-	// Use a persistence strategy
-	persistenceStrategy, err := postgres.NewPostgresStrategy(s.payloadTransformer)
-	s.Require().NoError(err, "failed initializing persistent strategy")
-
-	// Create message factory
-	messageFactory, err := eventStoreSQL.NewAggregateChangedFactory(s.payloadTransformer)
-	s.Require().NoError(err, "failed on dependencies load")
-
-	// Create event store
-	s.eventStore, err = postgres.NewEventStore(persistenceStrategy, db, messageFactory, nil)
-	s.Require().NoError(err, "failed on dependencies load")
-
-	// Create the event stream
 	ctx := context.Background()
-	err = s.eventStore.Create(ctx, s.eventStream)
-	s.Require().NoError(err, "failed on create event stream")
-
-	// Setup the projection tables etc.
-	eventStreamTable, err := persistenceStrategy.GenerateTableName(s.eventStream)
-	s.Require().NoError(err, "failed to generate eventstream table name")
-
-	queries := postgres.StreamProjectorCreateSchema("projections", s.eventStream, eventStreamTable)
+	queries := postgres.StreamProjectorCreateSchema("projections", s.eventStream, s.eventStoreTable)
 	for _, query := range queries {
-		_, err := db.ExecContext(ctx, query)
+		_, err := s.DB().ExecContext(ctx, query)
 		s.Require().NoError(err, "failed to create projection tables etc.")
 	}
 }
@@ -306,147 +268,4 @@ func (s *streamProjectorTestSuite) expectProjectionState(name string, expectedPo
 	}
 
 	s.Require().Equal(expectedPosition, position, "failed to fetch expected projection state")
-}
-
-func (s *streamProjectorTestSuite) appendEvents(events map[aggregate.ID][]interface{}) {
-	ctx := context.Background()
-	for aggID, aggEvents := range events {
-		// Find the last event
-		matcher := metadata.WithConstraint(
-			metadata.WithConstraint(
-				metadata.NewMatcher(),
-				aggregate.TypeKey,
-				metadata.Equals,
-				"account",
-			),
-			aggregate.IDKey,
-			metadata.Equals,
-			aggID,
-		)
-		stream, err := s.eventStore.Load(ctx, s.eventStream, 0, nil, matcher)
-		s.Require().NoError(err)
-
-		var lastVersion int
-		for stream.Next() {
-			msg, _, err := stream.Message()
-			s.Require().NoError(err)
-
-			lastVersion = int(msg.Metadata().Value(aggregate.VersionKey).(float64))
-		}
-
-		// Transform the events into messages
-		messages := make([]messaging.Message, len(aggEvents))
-		for i, event := range aggEvents {
-			m := metadata.WithValue(
-				metadata.WithValue(
-					metadata.WithValue(metadata.New(), aggregate.IDKey, aggID),
-					aggregate.VersionKey,
-					lastVersion+i+1,
-				),
-				aggregate.TypeKey,
-				"account",
-			)
-
-			message, err := aggregate.ReconstituteChange(
-				aggID,
-				messaging.GenerateUUID(),
-				event,
-				m,
-				time.Now().UTC(),
-				uint(i+1),
-			)
-			s.Require().NoError(err, "failed on create messages")
-
-			messages[i] = message
-		}
-
-		// Append the messages to the stream
-		err = s.eventStore.AppendTo(ctx, s.eventStream, messages)
-		s.Require().NoError(err, "failed to append messages")
-	}
-}
-
-// waitTimeout waits for the waitgroup for the specified max timeout.
-// Returns true if waiting timed out.
-func (s *streamProjectorTestSuite) waitTimeout(wg *sync.WaitGroup, timeout time.Duration) bool {
-	c := make(chan struct{})
-	go func() {
-		defer close(c)
-		wg.Wait()
-	}()
-	select {
-	case <-c:
-		return false // completed normally
-	case <-time.After(timeout):
-		return true // timed out
-	}
-}
-
-var (
-	_ eventstore.Projection = &DepositedProjection{}
-)
-
-type (
-	AccountCredited struct {
-		Amount uint
-	}
-
-	AccountDeposited struct {
-		Amount uint
-	}
-
-	DepositedProjection struct {
-	}
-
-	depositedProjectionState struct {
-		Total       uint
-		TotalAmount uint64
-	}
-)
-
-func (p *DepositedProjection) Init(ctx context.Context) (interface{}, error) {
-	return depositedProjectionState{}, nil
-}
-
-func (p *DepositedProjection) Name() string {
-	return "deposited_report"
-}
-
-func (p *DepositedProjection) FromStream() eventstore.StreamName {
-	return "event_stream"
-}
-
-func (p *DepositedProjection) Handlers() map[string]eventstore.ProjectionHandler {
-	return map[string]eventstore.ProjectionHandler{
-		"account_debited": func(ctx context.Context, state interface{}, message messaging.Message) (interface{}, error) {
-			projectionState := state.(depositedProjectionState)
-
-			switch event := message.Payload().(type) {
-			case AccountDeposited:
-				projectionState.Total++
-				projectionState.TotalAmount += uint64(event.Amount)
-			default:
-				return state, errors.New("unexpected message payload type")
-			}
-
-			return projectionState, nil
-		},
-	}
-}
-
-func (p *DepositedProjection) ReconstituteState(data []byte) (interface{}, error) {
-	var state depositedProjectionState
-	if err := json.Unmarshal(data, &state); err != nil {
-		return nil, err
-	}
-
-	return state, nil
-}
-
-func (p *DepositedProjection) Reset(ctx context.Context) error {
-	return nil
-}
-
-func (p *DepositedProjection) Delete(ctx context.Context) error {
-	return nil
 }
