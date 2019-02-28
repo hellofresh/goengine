0e81f65b8f6c56503625edd5d8437f36e2f8d304
warnar boekkooi <wbo@hellofresh.com>
2019-02-28 10:07:00 +0100

    Replace Mockery with Mock
    Mockery requires all func's to be typed which can lead to some very annoying issues.
    To avoid these issue testify mock will be replaced with github.com/golang/mock

--- mocks/mocks.go
+++ mocks/mocks.go
@@ -1,28 +1,62 @@
 package mocks
 
-import "github.com/stretchr/testify/mock"
+import (
+	"time"
+
+	"github.com/hellofresh/goengine"
+	"github.com/hellofresh/goengine/metadata"
+)
 
 // In order to make sure that we have the same mocks we can regenerate them using `go generate`
-//go:generate go run ../vendor/github.com/vektra/mockery/cmd/mockery/mockery.go -dir ../ -name=EventStore -outpkg mocks -output ./ -case underscore
-//go:generate go run ../vendor/github.com/vektra/mockery/cmd/mockery/mockery.go -dir ../ -name=EventStream -outpkg mocks -output ./ -case underscore
-//go:generate go run ../vendor/github.com/vektra/mockery/cmd/mockery/mockery.go -dir ../ -name=MessagePayloadFactory -outpkg mocks -output ./ -case underscore
-//go:generate go run ../vendor/github.com/vektra/mockery/cmd/mockery/mockery.go -dir ../ -name=MessagePayloadConverter -outpkg mocks -output ./ -case underscore
-//go:generate go run ../vendor/github.com/vektra/mockery/cmd/mockery/mockery.go -dir ../ -name=MessagePayloadResolver -outpkg mocks -output ./ -case underscore
-//go:generate go run ../vendor/github.com/vektra/mockery/cmd/mockery/mockery.go -dir ../driver/sql/ -name=PersistenceStrategy -outpkg sql -output ./driver/sql -case underscore
-//go:generate go run ../vendor/github.com/vektra/mockery/cmd/mockery/mockery.go -dir ../ -name=Query -outpkg mocks -output ./ -case underscore
-//go:generate go run ../vendor/github.com/vektra/mockery/cmd/mockery/mockery.go -dir ../driver/sql/ -name=MessageFactory -outpkg sql -output ./driver/sql -case underscore
-//go:generate go run ../vendor/github.com/vektra/mockery/cmd/mockery/mockery.go -dir ../ -name=Message -outpkg mocks -output ./ -case underscore
+//go:generate go run ../vendor/github.com/golang/mock/mockgen/ -package=mocks -destination event_store.go -mock_names EventStore=EventStore github.com/hellofresh/goengine EventStore
+//go:generate go run ../vendor/github.com/golang/mock/mockgen/ -package=mocks -destination event_stream.go -mock_names EventStream=EventStream github.com/hellofresh/goengine EventStream
+//go:generate go run ../vendor/github.com/golang/mock/mockgen/ -package=mocks -destination message_payload_factory.go -mock_names MessagePayloadFactory=MessagePayloadFactory github.com/hellofresh/goengine MessagePayloadFactory
+//go:generate go run ../vendor/github.com/golang/mock/mockgen/ -package=mocks -destination message_payload_converter.go -mock_names MessagePayloadConverter=MessagePayloadConverter github.com/hellofresh/goengine MessagePayloadConverter
+//go:generate go run ../vendor/github.com/golang/mock/mockgen/ -package=mocks -destination message_payload_resolver.go -mock_names MessagePayloadResolver=MessagePayloadResolver github.com/hellofresh/goengine MessagePayloadResolver
+//go:generate go run ../vendor/github.com/golang/mock/mockgen/ -package=mocks -destination query.go -mock_names Query=Query github.com/hellofresh/goengine Query
+//go:generate go run ../vendor/github.com/golang/mock/mockgen/ -package=mocks -destination message.go -mock_names Message=Message github.com/hellofresh/goengine Message
+//go:generate go run ../vendor/github.com/golang/mock/mockgen/ -package=aggregate -destination aggregate/aggregate.go -mock_names Root=Root github.com/hellofresh/goengine/aggregate Root
+//go:generate go run ../vendor/github.com/golang/mock/mockgen/ -package=aggregate -destination aggregate/aggregate_another.go -mock_names Root=AnotherRoot github.com/hellofresh/goengine/aggregate Root
+//go:generate go run ../vendor/github.com/golang/mock/mockgen/ -package=sql -destination driver/sql/persistence_strategy.go -mock_names PersistenceStrategy=PersistenceStrategy github.com/hellofresh/goengine/driver/sql PersistenceStrategy
+//go:generate go run ../vendor/github.com/golang/mock/mockgen/ -package=sql -destination driver/sql/message_factory.go -mock_names MessageFactory=MessageFactory github.com/hellofresh/goengine/driver/sql MessageFactory
 
-// FetchFuncCalls a helper go fetch all calls made to a func
-func FetchFuncCalls(calls []mock.Call, funcName string) []mock.Call {
-	funcCalls := make([]mock.Call, 0, len(calls))
-	for _, call := range calls {
-		if call.Method != funcName {
-			continue
-		}
+var _ goengine.Message = &DummyMessage{}
 
-		funcCalls = append(funcCalls, call)
+type DummyMessage struct {
+	uuid      goengine.UUID
+	payload   interface{}
+	metadata  metadata.Metadata
+	createdAt time.Time
+}
+
+func NewDummyMessage(id goengine.UUID, payload interface{}, meta metadata.Metadata, time time.Time) *DummyMessage {
+	return &DummyMessage{
+		id,
+		payload,
+		meta,
+		time,
 	}
+}
 
-	return funcCalls
+func (d *DummyMessage) UUID() goengine.UUID {
+	return d.uuid
+}
+
+func (d *DummyMessage) CreatedAt() time.Time {
+	return d.createdAt
+}
+
+func (d *DummyMessage) Payload() interface{} {
+	return d.payload
+}
+
+func (d *DummyMessage) Metadata() metadata.Metadata {
+	return d.metadata
+}
+
+func (d *DummyMessage) WithMetadata(key string, value interface{}) goengine.Message {
+	newMessage := *d
+	newMessage.metadata = metadata.WithValue(d.metadata, key, value)
+
+	return &newMessage
 }
