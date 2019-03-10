c6a867a3b8aba2edea0e12e8960e957af1c3bfa0
warnar boekkooi <wbo@hellofresh.com>
2019-03-10 22:58:50 +0100

    Update logrus logger

--- extension/logrus/logrus.go
+++ extension/logrus/logrus.go
@@ -1,6 +1,8 @@
 package logrus
 
 import (
+	"sync/atomic"
+
 	"github.com/hellofresh/goengine"
 	"github.com/sirupsen/logrus"
 )
@@ -11,51 +13,96 @@ type wrapper struct {
 
 // Wrap wraps a logrus.Logger
 func Wrap(logger *logrus.Logger) goengine.Logger {
-	return wrapper{entry: logrus.NewEntry(logger)}
+	return &wrapper{entry: logrus.NewEntry(logger)}
 }
 
 // WrapEntry wraps a logrus.Entry
 func WrapEntry(entry *logrus.Entry) goengine.Logger {
-	return wrapper{entry: entry}
+	return &wrapper{entry: entry}
 }
 
 // StandardLogger return a wrapped version of the logrus.StandardLogger()
 func StandardLogger() goengine.Logger {
 	var origLogger = logrus.StandardLogger()
-	return wrapper{entry: logrus.NewEntry(origLogger)}
+	return &wrapper{entry: logrus.NewEntry(origLogger)}
 }
 
-// Error writes a log with log level error
-func (w wrapper) Error(msg string) {
-	w.entry.Error(msg)
+func (w wrapper) Error(msg string, fields func(goengine.LoggerEntry)) {
+	if fields == nil {
+		w.entry.Error(msg)
+		return
+	}
+
+	if w.enabled(logrus.ErrorLevel) {
+		w.entry.WithFields(fieldsToMap(fields)).Error(msg)
+	}
 }
 
-// Warn writes a log with log level warn
-func (w wrapper) Warn(msg string) {
-	w.entry.Warn(msg)
+func (w wrapper) Warn(msg string, fields func(goengine.LoggerEntry)) {
+	if fields == nil {
+		w.entry.Warn(msg)
+		return
+	}
+
+	if w.enabled(logrus.WarnLevel) {
+		w.entry.WithFields(fieldsToMap(fields)).Warn(msg)
+	}
 }
 
-// Info writes a log with log level info
-func (w wrapper) Info(msg string) {
-	w.entry.Warn(msg)
+func (w wrapper) Info(msg string, fields func(goengine.LoggerEntry)) {
+	if fields == nil {
+		w.entry.Info(msg)
+		return
+	}
+
+	if w.enabled(logrus.InfoLevel) {
+		w.entry.WithFields(fieldsToMap(fields)).Info(msg)
+	}
 }
 
-// Debug writes a log with log level debug
-func (w wrapper) Debug(msg string) {
-	w.entry.Debug(msg)
+func (w wrapper) Debug(msg string, fields func(goengine.LoggerEntry)) {
+	if fields == nil {
+		w.entry.Debug(msg)
+		return
+	}
+
+	if w.enabled(logrus.DebugLevel) {
+		w.entry.WithFields(fieldsToMap(fields)).Debug(msg)
+	}
 }
 
-// WithError Add an error as single field to the log entry
-func (w wrapper) WithError(err error) goengine.Logger {
-	return wrapper{entry: w.entry.WithError(err)}
+func (w *wrapper) WithFields(fields func(goengine.LoggerEntry)) goengine.Logger {
+	if fields == nil {
+		return w
+	}
+
+	return &wrapper{entry: w.entry.WithFields(fieldsToMap(fields))}
 }
 
-// WithField Adds a field to the log entry
-func (w wrapper) WithField(key string, val interface{}) goengine.Logger {
-	return wrapper{entry: w.entry.WithField(key, val)}
+func (w wrapper) enabled(level logrus.Level) bool {
+	return logrus.Level(atomic.LoadUint32((*uint32)(&w.entry.Logger.Level))) >= level
 }
 
-//WithFields Adds a set of fields to the log entry
-func (w wrapper) WithFields(fields goengine.Fields) goengine.Logger {
-	return wrapper{entry: w.entry.WithFields(logrus.Fields(fields))}
+func fieldsToMap(fields func(goengine.LoggerEntry)) logrus.Fields {
+	e := entry{}
+	fields(e)
+	return logrus.Fields(e)
+}
+
+type entry map[string]interface{}
+
+func (e entry) Int(k string, v int) {
+	e[k] = v
+}
+
+func (e entry) String(k, v string) {
+	e[k] = v
+}
+
+func (e entry) Error(err error) {
+	e["error"] = err
+}
+
+func (e entry) Any(k string, v interface{}) {
+	e[k] = v
 }
