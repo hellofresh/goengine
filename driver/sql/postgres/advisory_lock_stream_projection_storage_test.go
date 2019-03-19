69767a873bad6c2cd68bdfd7ac7965ecb375c056
warnar boekkooi <wbo@hellofresh.com>
2019-03-19 10:54:04 +0100

    Add Stream CreateProjection test

--- driver/sql/postgres/advisory_lock_stream_projection_storage_test.go
+++ driver/sql/postgres/advisory_lock_stream_projection_storage_test.go
@@ -1,90 +1,47 @@
 // +build unit
 
-package postgres
+package postgres_test
 
 import (
 	"context"
-	"database/sql"
 	"errors"
 	"testing"
 
-	sqlmock "github.com/DATA-DOG/go-sqlmock"
+	"github.com/golang/mock/gomock"
 	"github.com/hellofresh/goengine"
-	driverSQL "github.com/hellofresh/goengine/driver/sql"
-	"github.com/hellofresh/goengine/driver/sql/internal/test"
+	"github.com/hellofresh/goengine/driver/sql/postgres"
+	"github.com/hellofresh/goengine/mocks/driver/sql"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestAdvisoryLockStreamProjectionStorage_PersistState(t *testing.T) {
-	mockedProjectionState := "I'm a projection state"
-	notification := &driverSQL.ProjectionNotification{
-		No:          1,
-		AggregateID: "d8490e85-dd22-4c32-9cb0-a29e57949951",
-	}
-	projectionState := driverSQL.ProjectionState{
-		Position:        5,
-		ProjectionState: mockedProjectionState,
-	}
+func TestAdvisoryLockStreamProjectionStorage_CreateProjection(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
-	test.RunWithMockDB(t, "Persist projection state", func(t *testing.T, db *sql.DB, dbMock sqlmock.Sqlmock) {
-		stateEncoderCalls := 0
-		stateEncoder := func(i interface{}) (bytes []byte, e error) {
-			assert.Equal(t, mockedProjectionState, i)
+	storage, err := postgres.NewAdvisoryLockStreamProjectionStorage(
+		"my_projection",
+		"event_store_projection",
+		sql.NewProjectionStateSerialization(ctrl),
+		goengine.NopLogger,
+	)
+	require.NoError(t, err)
 
-			stateEncoderCalls++
-			return []byte(mockedProjectionState), nil
-		}
+	// Test no error
+	ctx := context.Background()
+	mockDB := sql.NewExecer(ctrl)
+	mockDB.EXPECT().ExecContext(ctx, gomock.AssignableToTypeOf(""), "my_projection").Times(1).Return(nil, nil)
 
-		conn, err := db.Conn(context.Background())
-		require.NoError(t, err)
+	err = storage.CreateProjection(ctx, mockDB)
+	assert.NoError(t, err)
 
-		storage, err := NewAdvisoryLockStreamProjectionStorage("my_projection", "projection_table", stateEncoder, goengine.NopLogger)
-		require.NoError(t, err)
+	// Test DB error
+	expectedErr := errors.New("test error")
 
-		dbMock.ExpectExec("^UPDATE \"projection_table\" SET").
-			WithArgs(5, []byte(mockedProjectionState), "my_projection").
-			WillReturnResult(sqlmock.NewResult(18, 1))
+	ctx = context.Background()
+	mockDB = sql.NewExecer(ctrl)
+	mockDB.EXPECT().ExecContext(ctx, gomock.AssignableToTypeOf(""), "my_projection").Times(1).Return(nil, expectedErr)
 
-		err = storage.PersistState(conn, notification, projectionState)
-		assert.NoError(t, err)
-		assert.Equal(t, 1, stateEncoderCalls)
-	})
-
-	test.RunWithMockDB(t, "Persist projection state (using default encoder)", func(t *testing.T, db *sql.DB, dbMock sqlmock.Sqlmock) {
-		projectionState := driverSQL.ProjectionState{
-			Position:        6,
-			ProjectionState: nil,
-		}
-
-		conn, err := db.Conn(context.Background())
-		require.NoError(t, err)
-
-		storage, err := NewAdvisoryLockStreamProjectionStorage("my_projection", "projection_table", nil, goengine.NopLogger)
-		require.NoError(t, err)
-
-		dbMock.ExpectExec("^UPDATE \"projection_table\" SET").
-			WithArgs(6, []byte("{}"), "my_projection").
-			WillReturnResult(sqlmock.NewResult(18, 1))
-
-		err = storage.PersistState(conn, notification, projectionState)
-		assert.NoError(t, err)
-	})
-
-	test.RunWithMockDB(t, "Fail when state encoding fails", func(t *testing.T, db *sql.DB, dbMock sqlmock.Sqlmock) {
-		stateEncoderErr := errors.New("Failed to encode")
-		stateEncoder := func(i interface{}) (bytes []byte, e error) {
-			return nil, stateEncoderErr
-		}
-
-		conn, err := db.Conn(context.Background())
-		require.NoError(t, err)
-
-		storage, err := NewAdvisoryLockStreamProjectionStorage("my_projection", "projection_table", stateEncoder, goengine.NopLogger)
-		require.NoError(t, err)
-
-		err = storage.PersistState(conn, notification, projectionState)
-		assert.Equal(t, stateEncoderErr, err)
-	})
-
+	err = storage.CreateProjection(ctx, mockDB)
+	assert.Equal(t, expectedErr, err)
 }
