cd3d721c786ed639e4306a35cf678791c4d8873c
Muhammad Bilal Javed <m.bilaljaved@gmail.com>
2019-06-13 15:52:38 +0200

    Fix goimport

--- driver/sql/metrics_nop.go
+++ driver/sql/metrics_nop.go
@@ -5,7 +5,8 @@ var NopMetrics Metrics = &nopMetrics{}
 
 type nopMetrics struct{}
 
-func (nm *nopMetrics) ReceivedNotification(isNotification bool) {}
-func (nm *nopMetrics) QueueNotification(notification *ProjectionNotification) {}
+func (nm *nopMetrics) ReceivedNotification(isNotification bool)                         {}
+func (nm *nopMetrics) QueueNotification(notification *ProjectionNotification)           {}
 func (nm *nopMetrics) StartNotificationProcessing(notification *ProjectionNotification) {}
-func (nm *nopMetrics) FinishNotificationProcessing(notification *ProjectionNotification, success bool) {}
+func (nm *nopMetrics) FinishNotificationProcessing(notification *ProjectionNotification, success bool) {
+}
